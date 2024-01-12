db79741206d80fff0d5d514e5df8628acc85ef9b
Mukund-Tandon <mukundtandon1211@gmail.com>
2024-01-12 21:47:07 +0530

    added  files

--- week-4/react-basics/learn/src/App.jsx
+++ week-4/react-basics/learn/src/App.jsx
@@ -4,14 +4,38 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [toDos] = useState([])
+  function addTodo(){
+    let title = document.getElementById('title');
+    let desc = document.getElementById('desc');
+    let todoList = document.getElementById('todos');
+    console.log(title.value);
+    console.log(desc.value);
+    let newtodo = document.createElement('div');
+    
+    let ctitle = document.createElement('p');
+    ctitle.innerText = title.value;
+    
+    let cdesc = document.createElement('p');
+    cdesc.innerText = desc.value;
 
+    newtodo.appendChild(ctitle);
+    newtodo.appendChild(cdesc);
+
+    todoList.appendChild(newtodo);
+
+  }
   return (
-    <>
+    
       <div>
-        <h1>React + Vite ⚡️</h1>
+        <input type ='text' placeholder='title' id='title'></input>
+        <br />
+        <input type ='text' placeholder='description' id='desc'></input>
+        <br />
+        <button onClick={addTodo} title='add'>Add</button>
+        <div id="todos"></div>
       </div>
-    </>
+    
   )
 }
 
